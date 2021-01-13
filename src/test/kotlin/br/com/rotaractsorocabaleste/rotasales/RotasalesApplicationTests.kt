abec541d86a7d4c7197847126bdb7b21fcb1cfd4
Gabriel Felicidade <gabriel.felicidade@gobots.com.br>
2021-01-13 16:35:53 -0300

    Initial commit

---
+++ src/test/kotlin/br/com/rotaractsorocabaleste/rotasales/RotasalesApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.rotaractsorocabaleste.rotasales
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RotasalesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
