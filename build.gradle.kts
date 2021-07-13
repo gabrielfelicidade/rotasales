c54a7af8cd1b79376732c56aaa2ef7f591ad24d1
Gabriel Felicidade <gabriel.felicidade@etec.sp.gov.br>
2021-07-13 01:00:22 -0300

    updated version of kotlin and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.3.5.RELEASE"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.3.72"
-	kotlin("plugin.spring") version "1.3.72"
+	id("org.springframework.boot") version "2.5.2"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.5.20"
+	kotlin("plugin.spring") version "1.5.20"
+	kotlin("plugin.jpa") version "1.5.20"
 }
 
 group = "br.com.rotaractsorocabaleste"
@@ -22,7 +23,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
 	implementation("org.flywaydb:flyway-core")
 	implementation("com.itextpdf:itextpdf:5.5.13.2")
 	implementation("com.itextpdf:io:7.1.14")
