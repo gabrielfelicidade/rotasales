d6d24bc4229875834b8e697215a1b021414a5c64
unknown <Gabriel Felicidade>
2022-07-08 21:28:58 -0300

    trying to solve runtime problem

--- src/test/java/br/com/rotaractsorocabaleste/rotasales/RotasalesApplicationTests.java
+++ src/test/java/br/com/rotaractsorocabaleste/rotasales/RotasalesApplicationTests.java
@@ -2,8 +2,11 @@ package br.com.rotaractsorocabaleste.rotasales;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class RotasalesApplicationTests {
 
 	@Test
